c4e15c50614b32196401bc9910f12e1c36d285a9
Pavel Senchenko <pavel.senchenko@gmail.com>
2023-10-24 10:18:37 +0300

    Initial Commit

---
+++ BookSummarizer/BookSummarizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSummarizerApp.swift
+//  BookSummarizer
+//
+//  Created by Pavlo Senchenko on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookSummarizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookSummarizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookSummarizer
+//
+//  Created by Pavlo Senchenko on 24.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
